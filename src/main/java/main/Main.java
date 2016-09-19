f2e5e229b79e4cbfce8c23d6926f3dd57f6d2ddd
LucasDelvallet <lucas.delvallet@hotmail.fr>
2016-09-19 17:19:39 +0200

    * Preuve de fonctionnement de l'analyse de pull request.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,14 +4,14 @@ import java.io.IOException;
 
 import org.kohsuke.github.GHIssueState;
 import org.kohsuke.github.GHPullRequest;
+import org.kohsuke.github.GHPullRequestFileDetail;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
+import org.kohsuke.github.PagedIterable;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World !");
-		
 		try {
 			GitHub github = GitHub.connect();
 			
@@ -19,9 +19,16 @@ public class Main {
 			
 			for(GHPullRequest PR : repo.getPullRequests(GHIssueState.OPEN)){
 				System.out.println("Titre : " + PR.getTitle());
+				
+
+				System.out.println("	Liste des fichiers d'extension .java modifié/ajouté/supprimé : ");
+				for(GHPullRequestFileDetail filesDetail : PR.listFiles()){
+					String fileName = filesDetail.getFilename();
+					if(fileName.endsWith(".java")){
+						System.out.println("		Nom du fichier : " + fileName);
+					}
+				}
 			}
-			
-			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
