9b86d81f4dca02728f14308814252dce7b4e4a7a
Delvallet Lucas <delvallet@b10p4.fil.univ-lille1.fr>
2016-09-26 09:38:31 +0200

    Ajout d'une classe prAnalyzer

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,76 +1,33 @@
 package main;
 
-import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.kohsuke.github.GHIssueState;
-import org.kohsuke.github.GHPullRequest;
-import org.kohsuke.github.GHPullRequestFileDetail;
-import org.kohsuke.github.GHRepository;
-import org.kohsuke.github.GitHub;
-import org.kohsuke.github.PagedIterable;
+import prAnalyzer.PRAnalyzer;
 
 public class Main {
 
 	public static void main(String[] args) {
-		try {
-			Pattern methodAdded = Pattern.compile("\\+[ ]*[a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [a-zA-Z]+ *\\([a-zA-Z, ]*\\) *\\{");
-			Pattern methodDeleted = Pattern.compile("\\-[ ]*[a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [a-zA-Z]+ *\\([a-zA-Z, ]*\\) *\\{");
-			Pattern commentAdded = Pattern.compile("\\+[ ]*(/\\*([^*]|[\\r\\n]|(\\*+([^*/]|[\\r\\n])))*\\*+/)|\\+[ ]*(//.*)");
-			Matcher m;
-			
-			GitHub github = GitHub.connect();
-			
-			GHRepository repo = github.getRepository("structurizr/java");
-			
-			for(GHPullRequest PR : repo.getPullRequests(GHIssueState.OPEN)){
-				System.out.println("Titre : " + PR.getTitle());				
-
-				System.out.println("	Liste des fichiers d'extension .java modifiés/ajoutés/supprimés : ");
-				for(GHPullRequestFileDetail filesDetail : PR.listFiles()){
-					String fileName = filesDetail.getFilename();
-					if(fileName.endsWith(".java")){
-						System.out.println("		Nom du fichier : " + fileName);
-						
-						m = methodAdded.matcher(filesDetail.getPatch()); 					//On donne le pattern et le string
-						System.out.println("			Méthodes ajoutées/modifiées : "); 	//On indique ce qu'on va afficher
-						while(m.find()) { 													//On trouve la prochaine chaine qui satisfait le pattern
-							String prototype = m.group().replaceAll("(\\+ *)|( *\\{)", ""); //On retire les caractères inutiles qui ne font pas parti du prototype de la méthode
-							System.out.println("				" + prototype); 			//On affiche la méthode.
-						}
-						
-						//On recommence pour les méthodes supprimées
-						m = methodDeleted.matcher(filesDetail.getPatch());
-						System.out.println("			Méthodes supprimées : ");
-						while(m.find()) {
-							String prototype = m.group().replaceAll("(\\- *)|( *\\{)", "");
-							System.out.println("				" + prototype);
-						}
-						
-						//On compte le nombre de commentaires ajoutés et on le montre
-						m = methodAdded.matcher(filesDetail.getPatch());
-						int count = 0;
-						while (m.find())
-						    count++;
-						System.out.println("			Nombre de commentaires ajoutés : " + count);
-					}
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		PRAnalyzer analyzer = new PRAnalyzer("structurizr/java");
 		
+		int indexPR = 0;
+		int indexFile = 10;
+		int indexNewMethod = 1;
+		
+		System.out.println("Nombre de pull request 					: " + analyzer.getNumberOfPullRequests());
+		System.out.println("Nombre de fichier de la PR d'index 0			: " + analyzer.getNumberOfFiles(indexPR));
+		System.out.println("Nom du fichier d'index 1 				: " + analyzer.getFileName(indexPR, indexFile));
+		System.out.println("Nombre de nouvelle mÃ©thode				: " + analyzer.getNumberOfNewMethodInFile(indexPR, indexFile));
+		System.out.println("Nom de la nouvelle mÃ©thode d'index 1			: " + analyzer.getNewMethodPrototype(indexPR, indexFile, indexNewMethod));
+		System.out.println("Nombre de mÃ©thode supprimÃ©e				: " + analyzer.getNumberOfDeletedMethodInFile(indexPR, indexFile));
+		System.out.println("Nombre de commentaire ajoutÃ©				: " + analyzer.getNumberOfNewComments(indexPR, indexFile));
+
 	}
 }
 
-//	Regex pour chopper différents trucs (pour les foutre dans des string il faut doubler les \\:
+//	Regex pour chopper diffÃ©rents trucs (pour les foutre dans des string il faut doubler les \\:
 //
-//	Les méthodes ajoutées ou supprimées :
+//	Les mÃ©thodes ajoutÃ©es ou supprimÃ©es :
 //		\+[ ]*[a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [a-zA-Z]+ *\([a-zA-Z, ]*\) *\{
 //		\-[ ]*[a-zA-Z]* [a-zA-Z]* [a-zA-Z]+ [a-zA-Z]+ *\([a-zA-Z, ]*\) *\{
 //	
-//	Détecter les commentaires ajoutés ou supprimés :
+//	DÃ©tecter les commentaires ajoutÃ©s ou supprimÃ©s :
 //		\+[ ]*(/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)|\+[ ]*(//.*)
 //		\-[ ]*(/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)|\-[ ]*(//.*)
-//
---
+++ src/main/java/prAnalyzer/PRAnalyzer.java
@@ -0,0 +1,175 @@
+package prAnalyzer;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.kohsuke.github.GHIssueState;
+import org.kohsuke.github.GHPullRequest;
+import org.kohsuke.github.GHPullRequestFileDetail;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+public class PRAnalyzer {
+
+	static private Pattern methodAdded = Pattern.compile("\\+[ ]*[a-zA-Z<>]* [a-zA-Z<>]* [a-zA-Z<>]+ [a-zA-Z<>]+ *\\([a-zA-Z, <>]*\\) *[a-zA-Z]* [a-zA-Z]* *\\{");
+	static private Pattern methodDeleted = Pattern.compile("\\-[ ]*[a-zA-Z<>]* [a-zA-Z<>]* [a-zA-Z<>]+ [a-zA-Z<>]+ *\\([a-zA-Z, <>]*\\) *[a-zA-Z]* [a-zA-Z]* *\\{");
+	static private Pattern commentAdded = Pattern.compile("\\+[ ]*(/\\*([^*]|[\\r\\n]|(\\*+([^*/]|[\\r\\n])))*\\*+/)|\\+[ ]*(//.*)");
+	
+	private GHRepository repo;
+	private List<GHPullRequest> pullRequests;
+	
+	public PRAnalyzer(String repoName){
+		try {
+			GitHub github = GitHub.connect();
+		
+			repo = github.getRepository(repoName);
+			
+			pullRequests = repo.getPullRequests(GHIssueState.OPEN);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int getNumberOfPullRequests(){
+		return pullRequests.size();
+	}
+	
+	public int getNumberOfFiles(int pullRequestIndex){
+		return pullRequests.get(pullRequestIndex).listFiles().asList().size();
+	}
+	
+	public String getFileName(int pullRequestIndex, int fileIndex){
+		return pullRequests.get(pullRequestIndex).listFiles().asList().get(fileIndex).getFilename();
+	}
+	
+	public int getNumberOfNewMethodInFile(int pullRequestIndex, int fileIndex){
+		Matcher m;
+		
+		GHPullRequestFileDetail fileDetail = pullRequests.get(pullRequestIndex).listFiles().asList().get(fileIndex);
+			
+		int count = 0;
+		if(fileDetail.getFilename().endsWith(".java")){
+			m = methodAdded.matcher(fileDetail.getPatch()); 
+			while(m.find()) { 		
+				count++;
+			}
+		}
+		
+		return count;
+	}
+	
+	public String getNewMethodPrototype(int pullRequestIndex, int fileIndex, int methodIndex){
+		Matcher m;
+		
+		GHPullRequestFileDetail fileDetail = pullRequests.get(pullRequestIndex).listFiles().asList().get(fileIndex);
+		
+		int count = 0;
+		if(fileDetail.getFilename().endsWith(".java")){
+			m = methodAdded.matcher(fileDetail.getPatch()); 					
+			
+			while(m.find()) { 		
+				if(count == methodIndex){
+					return m.group().replaceAll("(\\+ *)|( *\\{)", "");
+				}
+				count++;
+			}
+		}
+		
+		return "Error";
+	}
+	
+	public int getNumberOfDeletedMethodInFile(int pullRequestIndex, int fileIndex){
+		Matcher m;
+		
+		GHPullRequestFileDetail fileDetail = pullRequests.get(pullRequestIndex).listFiles().asList().get(fileIndex);
+		
+		int count = 0;
+		if(fileDetail.getFilename().endsWith(".java")){
+			m = methodDeleted.matcher(fileDetail.getPatch()); 	
+			while(m.find()) { 		
+				count++;
+			}
+		}
+		
+		return count;
+	}
+	
+	public String getDeletedMethodPrototype(int pullRequestIndex, int fileIndex, int methodIndex){
+		Matcher m;
+		
+		GHPullRequestFileDetail fileDetail = pullRequests.get(pullRequestIndex).listFiles().asList().get(fileIndex);
+		
+		if(fileDetail.getFilename().endsWith(".java")){
+			m = methodDeleted.matcher(fileDetail.getPatch()); 					
+			int count = 0;
+			while(m.find()) { 	
+				if(count == methodIndex){
+					return m.group().replaceAll("(\\+ *)|( *\\{)", "");
+				}
+				count++;
+			}
+		}
+		
+		return "Error";
+	}
+	
+	public int getNumberOfNewComments(int pullRequestIndex, int fileIndex){
+		Matcher m;
+		
+		GHPullRequestFileDetail fileDetail = pullRequests.get(pullRequestIndex).listFiles().asList().get(fileIndex);
+		
+		int count = 0;
+		if(fileDetail.getFilename().endsWith(".java")){
+			m = commentAdded.matcher(fileDetail.getPatch()); 	
+			while(m.find()) { 		
+				count++;
+			}
+		}
+		
+		return count;
+	}
+	
+	
+	public void temp(){
+		
+		Matcher m;
+		
+		for(GHPullRequest PR : pullRequests){
+			System.out.println("Titre : " + PR.getTitle());				
+
+			System.out.println("	Liste des fichiers d'extension .java modifiés/ajoutés/supprimés : ");
+			for(GHPullRequestFileDetail filesDetail : PR.listFiles()){
+				String fileName = filesDetail.getFilename();
+				if(fileName.endsWith(".java")){
+					System.out.println("		Nom du fichier : " + fileName);
+					
+					m = methodAdded.matcher(filesDetail.getPatch()); 					//On donne le pattern et le string
+					System.out.println("			Méthodes ajoutées/modifiées : "); 	//On indique ce qu'on va afficher
+					while(m.find()) { 													//On trouve la prochaine chaine qui satisfait le pattern
+						String prototype = m.group().replaceAll("(\\+ *)|( *\\{)", ""); //On retire les caractères inutiles qui ne font pas parti du prototype de la méthode
+						System.out.println("				" + prototype); 			//On affiche la méthode.
+					}
+					
+					//On recommence pour les méthodes supprimées
+					m = methodDeleted.matcher(filesDetail.getPatch());
+					System.out.println("			Méthodes supprimées : ");
+					while(m.find()) {
+						String prototype = m.group().replaceAll("(\\- *)|( *\\{)", "");
+						System.out.println("				" + prototype);
+					}
+					
+					//On compte le nombre de commentaires ajoutés et on le montre
+					m = commentAdded.matcher(filesDetail.getPatch());
+					int count = 0;
+					while (m.find())
+					    count++;
+					System.out.println("			Nombre de commentaires ajoutés : " + count);
+				}
+			}
+		}
+	}
+	
+	
+}
