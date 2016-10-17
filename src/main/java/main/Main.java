4b8c35dd8cf57f6835b162e3fd1c4b050df3eae1
Quentin Delvallet <quentin.delvallet@hotmail.fr>
2016-10-17 12:47:48 +0200

    More code refactoring

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,19 +5,18 @@ import spoonPRAnalyser.SpoonPRAnalyzer;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		if(args[1].equals("Analysis")){
+
+		if (args[1].equals("Analysis")) {
 			SpoonPRAnalyzer analyzer = new SpoonPRAnalyzer(args[0]);
 			analyzer.StartAnalysisOfRepo();
 			System.exit(0);
 		}
-		
-		if(args[1].equals("Bot")){
+
+		if (args[1].equals("Bot")) {
 			SpoonPRAnalyzer analyzer = new SpoonPRAnalyzer(args[0]);
-			analyzer.StartBot();	
+			analyzer.StartBot();
 			System.exit(0);
 		}
-		
 
 		System.exit(1);
 	}
